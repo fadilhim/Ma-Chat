f84cde4bab16aa4c67b922f9c3cd94c0b3c0b4cd
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-12 06:41:05 +0700

    add profile and location screen

---
+++ src/screens/HomePage/Profile.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ProfileScreen extends Component {
+
+    render() {
+        return (
+        <View style={styles.viewStyles}>
+            <Text style={styles.textStyles}>
+                MaChat
+            </Text>
+        </View>
+        );
+    }
+}
+
+const styles = {
+    viewStyles: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#207561'
+    },
+    textStyles: {
+        color: 'white',
+        fontSize: 40,
+        fontWeight: 'bold',
+    },
+}
+
+export default ProfileScreen;
