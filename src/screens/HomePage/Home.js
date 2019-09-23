20d24b5f2a2320011a7878d436e73625cc434de9
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-23 23:46:49 +0700

    Add last chat message

--- src/screens/HomePage/Home.js
+++ src/screens/HomePage/Home.js
@@ -1,82 +1,98 @@
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, FlatList, SafeAreaView, Image, PermissionsAndroid, StyleSheet } from 'react-native';
-import firebase from 'firebase'
-import AsyncStorage from '@react-native-community/async-storage'
-import Geolocation from 'react-native-geolocation-service'
-import { Spinner } from 'native-base'
-import { SearchBar } from 'react-native-elements'
+import firebase from 'firebase';
+import AsyncStorage from '@react-native-community/async-storage';
+import Geolocation from 'react-native-geolocation-service';
+import { Spinner } from 'native-base';
+import { SearchBar } from 'react-native-elements';
 import { ScrollView } from 'react-native-gesture-handler';
 
 class HomeScreen extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
     }
 
     state = {
         users: [],
         uid: '',
-        lastmsg: []
+        lastmsg: [],
+        receiverList: [],
     }
 
     UNSAFE_componentWillMount = async () => {
         await AsyncStorage.getItem('uid').then(
             (uid) => this.setState({
-                uid: uid
+                uid: uid,
             })
-        )
+        );
 
-        let dbRef = firebase.database().ref('users')
+        //get all user
+        let dbRef = firebase.database().ref('users');
         await dbRef.on('child_added', ( value ) => {
-            let person = value.val()
-            person.uid = value.key
+            let person = value.val();
+            person.uid = value.key;
             this.setState(( prevState ) => {
                 return {
-                    users: [...prevState.users, person]
-                }
-            })
+                    users: [...prevState.users, person],
+                };
+            });
 
-            let friendList = person.uid
-            let lastmsg = firebase.database().ref('messages').child(this.state.uid).child(friendList).orderByKey().limitToLast(1)
-            lastmsg.on('child_added', (mssg) => {
-                let lastmsg = this.state.lastmsg
-                lastmsg.push({[mssg.key]: mssg.val()})
+            //get reveicer friend
+            let list = firebase.database().ref('messages/').child(this.state.uid);
+            list.on('value', (mssg) => {
                 this.setState({
-                    lastmsg
+                    receiverList: Object.keys(mssg.val())
+                });
+            });
+
+            //get latest message
+            let friendList = person.uid;
+            let lastmsg = firebase.database().ref('messages/').child(this.state.uid).child(friendList).orderByKey().limitToLast(1);
+            lastmsg.on('value', (mssg) => {
+                lastmsg.on('child_added', (ress) => {
+                    let lastmsgList = this.state.lastmsg;
+                    if(mssg.val() != null){
+                        lastmsgList.push({[mssg.key]: ress.val()});
+                        this.setState({
+                            lastmsgList,
+                        });
+                    }
                 })
-                // console.log(this.state.lastmsg)
-            })
-        })
+                
+            });
+
+        });
     }
 
     componentDidMount = async () => {
-        let hasLocationPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
-        if(!hasLocationPermission){
-            hasLocationPermission = await this.requestLocationPermission()
+        let hasLocationPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+        if (!hasLocationPermission){
+            hasLocationPermission = await this.requestLocationPermission();
         } else {
             Geolocation.watchPosition(
                 (position) => {
                     let userPosition = {
                         latitude: position.coords.latitude,
-                        longitude: position.coords.longitude
-                    }
+                        longitude: position.coords.longitude,
+                    };
                     firebase.database().ref('users/' + this.state.uid).update({
                         position: userPosition,
-                        lastSeen: position.timestamp
-                    })
+                        lastSeen: position.timestamp,
+                    });
                 },
                 (error) => {
                     // See error code charts below.
                     console.warn(error.code, error.message);
                 },
                 { enableHighAccuracy: true, timeout: 15000, forceRequestLocation: true, maximumAge: 10000, distanceFilter: 1 }
-            )
+            );
         }
     }
 
     componentWillUnmount = () => {
-        Geolocation.stopObserving()
+        Geolocation.stopObserving();
     }
 
     requestLocationPermission = async () => {
@@ -86,67 +102,77 @@ class HomeScreen extends Component {
                 {
                 title: 'Ma-Chat Location Permission',
                 message:
-                    `Ma-Chat needs permission to get your location`,
+                    'Ma-Chat needs permission to get your location',
                     buttonNeutral: 'Ask Me Later',
                     buttonPositive: 'OK',
                     buttonNegative: 'Cancel',
                 },
             );
-            return granted === PermissionsAndroid.RESULTS.GRANTED
+            return granted === PermissionsAndroid.RESULTS.GRANTED;
         } catch (err) {
             console.warn(err);
-            return false
+            return false;
         }
     }
 
     _renderRow = ({item}) => {
-        let msg = this.state.lastmsg
-        // console.log(msg)
-        if (item.uid != this.state.uid ){
+        let msg = Object.values(this.state.lastmsg);
+        let listReceiver = this.state.receiverList
+        let uidReceiver = item.uid
+
+        if (listReceiver.includes(uidReceiver)) {
+            let kkk = []
+            msg.map( (value) => {
+                let isi = Object.values(value)
+                isi.uid = Object.keys(value)[0]
+
+                if (isi.uid == uidReceiver) {
+                    kkk.push(isi[0])
+                }
+            })
             return (
                 <TouchableOpacity style={{ padding: 10, flexDirection: 'row' }} onPress={ () => this.props.navigation.navigate('Chat', {item: item}) }>
                     <Image source={{uri: item.photo}} style={{height: 50, width: 50, borderRadius: 50}} />
                     <View style={{paddingLeft: 10 }}>
                         <Text style={{ fontSize: 15, color: 'white' }}>{item.fullname}</Text>
                         <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            {msg.map( (value) => {
-                                value._id  == item.uid ?
-                                <Text>{value.text}</Text> : <Text></Text>
-                            })}
+                            <Text style={styles.friendLastMsg}>{kkk[0].text}</Text>
                         </View>
                     </View>
                 </TouchableOpacity>
-            )
+            );
         }
     }
 
     render() {
-        return(
+        // console.log('lastmsg', this.state.lastmsg)
+        return (
             <SafeAreaView style={{backgroundColor: '#353839', flex: 1}}>
                 <View style={{height: 52, backgroundColor: '#353839', justifyContent: 'center'}}>
                     <Text style={{color: 'white', fontSize: 20, fontFamily: 'Roboto', marginLeft: 10}}>Chats</Text>
                 </View>
                 <ScrollView>
-                    <SearchBar
+                    {/* <SearchBar
                         placeholder= 'Search'
                         placeholderTextColor= 'grey'
                         // onChangeText={this.updateSearch}
                         // value={search}
                         containerStyle={styles.searchInput}
                         inputContainerStyle={styles.inputContainer}
-                    />
+                    /> */}
                     {this.state.users.length > 0 ?
-                        <FlatList 
+                        <FlatList
+                            key={this.state.receiverList.length}
                             data={this.state.users}
                             renderItem={this._renderRow}
                             keyExtractor={ (item) => item.username }
                         />
                         :
-                        <Spinner color='white' />
+                        <Spinner color="white" />
                     }
                 </ScrollView>
             </SafeAreaView>
-        )
+        );
     }
 }
 
@@ -166,6 +192,13 @@ const styles =  StyleSheet.create({
         alignSelf: 'center',
         // fontSize: 10
     },
-})
+    friendLastMsg: {
+        fontSize: 12,
+        color: 'grey',
+        marginTop: 1,
+        // paddingLeft: 5,
+    },
+});
 
 export default HomeScreen
+;
