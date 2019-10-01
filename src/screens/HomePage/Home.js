9a2812af9b627ef218e61ce1c6c43e29bd8ddb69
Poko Groot <PokoGroot@users.noreply.github.com>
2019-10-01 14:15:04 +0700

    Optimize code

--- src/screens/HomePage/Home.js
+++ src/screens/HomePage/Home.js
@@ -1,11 +1,10 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, FlatList, SafeAreaView, Image, PermissionsAndroid, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, SafeAreaView, Image, PermissionsAndroid, StyleSheet, ActivityIndicator } from 'react-native';
 import firebase from 'firebase';
 import AsyncStorage from '@react-native-community/async-storage';
 import Geolocation from 'react-native-geolocation-service';
-import { Spinner } from 'native-base';
-import { SearchBar } from 'react-native-elements';
+// import { Spinner } from 'native-base';
 import { ScrollView } from 'react-native-gesture-handler';
 
 class HomeScreen extends Component {
@@ -42,9 +41,11 @@ class HomeScreen extends Component {
             //get reveicer friend
             let list = firebase.database().ref('messages/').child(this.state.uid);
             list.on('value', (mssg) => {
-                this.setState({
-                    receiverList: Object.keys(mssg.val())
-                });
+                if(mssg.val() != null){
+                    this.setState({
+                        receiverList: Object.keys(mssg.val())
+                    });
+                }
             });
 
             //get latest message
@@ -53,12 +54,18 @@ class HomeScreen extends Component {
             lastmsg.on('value', (mssg) => {
                 lastmsg.on('child_added', (ress) => {
                     let lastmsgList = this.state.lastmsg;
+
                     if(mssg.val() != null){
+                        lastmsgList.map( (item) => {
+                            
+                        })
                         lastmsgList.push({[mssg.key]: ress.val()});
+                        console.log('listen',lastmsgList)
                         this.setState({
-                            lastmsgList,
+                            lastmsg: lastmsgList,
                         });
                     }
+
                 })
                 
             });
@@ -121,26 +128,29 @@ class HomeScreen extends Component {
         let uidReceiver = item.uid
 
         if (listReceiver.includes(uidReceiver)) {
-            let kkk = []
+            let messagess = []
             msg.map( (value) => {
-                let isi = Object.values(value)
-                isi.uid = Object.keys(value)[0]
+                let val = Object.values(value)
+                val.uid = Object.keys(value)[0]
 
-                if (isi.uid == uidReceiver) {
-                    kkk.push(isi[0])
+                if (val.uid == uidReceiver) {
+                    messagess.push(val[0])
                 }
             })
-            return (
-                <TouchableOpacity style={{ padding: 10, flexDirection: 'row' }} onPress={ () => this.props.navigation.navigate('Chat', {item: item}) }>
-                    <Image source={{uri: item.photo}} style={{height: 50, width: 50, borderRadius: 50}} />
-                    <View style={{paddingLeft: 10 }}>
-                        <Text style={{ fontSize: 15, color: 'white' }}>{item.fullname}</Text>
-                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                            <Text style={styles.friendLastMsg}>{kkk[0].text}</Text>
+            console.log('index', messagess)
+            if (messagess[0] != undefined){
+                return (
+                    <TouchableOpacity style={{ padding: 10, flexDirection: 'row' }} onPress={ () => this.props.navigation.navigate('Chat', {item: item}) }>
+                        <Image source={{uri: item.photo}} style={{height: 50, width: 50, borderRadius: 50}} />
+                        <View style={{paddingLeft: 10 }}>
+                            <Text style={{ fontSize: 15, color: 'white' }}>{item.fullname}</Text>
+                            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                <Text style={styles.friendLastMsg}>{messagess[0].text}</Text>
+                            </View>
                         </View>
-                    </View>
-                </TouchableOpacity>
-            );
+                    </TouchableOpacity>
+                );
+            }
         }
     }
 
@@ -151,26 +161,23 @@ class HomeScreen extends Component {
                 <View style={{height: 52, backgroundColor: '#353839', justifyContent: 'center'}}>
                     <Text style={{color: 'white', fontSize: 20, fontFamily: 'Roboto', marginLeft: 10}}>Chats</Text>
                 </View>
-                <ScrollView>
-                    {/* <SearchBar
-                        placeholder= 'Search'
-                        placeholderTextColor= 'grey'
-                        // onChangeText={this.updateSearch}
-                        // value={search}
-                        containerStyle={styles.searchInput}
-                        inputContainerStyle={styles.inputContainer}
-                    /> */}
-                    {this.state.users.length > 0 ?
+                {this.state.users.length > 0 ?
+                    this.state.lastmsg.length > 0 ?
+                    <ScrollView>
                         <FlatList
                             key={this.state.receiverList.length}
                             data={this.state.users}
                             renderItem={this._renderRow}
                             keyExtractor={ (item) => item.username }
                         />
+                    </ScrollView>
+                    :
+                    <Text>You haven't sent any messages. Let's start to connect with your friend!</Text>
                         :
-                        <Spinner color="white" />
-                    }
-                </ScrollView>
+                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+                        <ActivityIndicator size='large' color='white' />
+                    </View>
+                }
             </SafeAreaView>
         );
     }
